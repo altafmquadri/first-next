1ce4e098eec591b523851a64c16c83c3cc799781
Altaf Quadri <altafmquadri@gmail.com>
2020-03-14 19:41:54 -0400

    first next.js app

---
+++ .next/static/development/pages/contact.js
@@ -0,0 +1,102 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["static/development/pages/contact.js"],{
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fcontact&absolutePagePath=%2Fmnt%2Fc%2Fusers%2Faq56215%2Fdevelopment%2Ffirst-next%2Fpages%2Fcontact.js!./":
+/*!************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fcontact&absolutePagePath=%2Fmnt%2Fc%2Fusers%2Faq56215%2Fdevelopment%2Ffirst-next%2Fpages%2Fcontact.js ***!
+  \************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/contact", function() {
+      var mod = __webpack_require__(/*! ./pages/contact.js */ "./pages/contact.js")
+      if(true) {
+        module.hot.accept(/*! ./pages/contact.js */ "./pages/contact.js", function() {
+          if(!next.router.components["/contact"]) return
+          var updatedPage = __webpack_require__(/*! ./pages/contact.js */ "./pages/contact.js")
+          next.router.update("/contact", updatedPage)
+        })
+      }
+      return mod
+    }]);
+  
+
+/***/ }),
+
+/***/ "./node_modules/react/index.js":
+/*!*******************************************************************************************!*\
+  !*** delegated ./node_modules/react/index.js from dll-reference dll_82519ec661270f7f484f ***!
+  \*******************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(/*! dll-reference dll_82519ec661270f7f484f */ "dll-reference dll_82519ec661270f7f484f"))("./node_modules/react/index.js");
+
+/***/ }),
+
+/***/ "./pages/contact.js":
+/*!**************************!*\
+  !*** ./pages/contact.js ***!
+  \**************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/mnt/c/users/aq56215/development/first-next/pages/contact.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+var ContactPage = function ContactPage() {
+  return __jsx("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3
+    },
+    __self: this
+  }, __jsx("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    },
+    __self: this
+  }, "Contact Page"), __jsx("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }, "Do not contact us ever!"));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (ContactPage);
+
+/***/ }),
+
+/***/ 3:
+/*!****************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2Fcontact&absolutePagePath=%2Fmnt%2Fc%2Fusers%2Faq56215%2Fdevelopment%2Ffirst-next%2Fpages%2Fcontact.js ***!
+  \****************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2Fcontact&absolutePagePath=%2Fmnt%2Fc%2Fusers%2Faq56215%2Fdevelopment%2Ffirst-next%2Fpages%2Fcontact.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fcontact&absolutePagePath=%2Fmnt%2Fc%2Fusers%2Faq56215%2Fdevelopment%2Ffirst-next%2Fpages%2Fcontact.js!./");
+
+
+/***/ }),
+
+/***/ "dll-reference dll_82519ec661270f7f484f":
+/*!*******************************************!*\
+  !*** external "dll_82519ec661270f7f484f" ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = dll_82519ec661270f7f484f;
+
+/***/ })
+
+},[[3,"static/runtime/webpack.js"]]]);
+//# sourceMappingURL=contact.js.map
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+const Index = () => {
+    console.log('here')
+    return (
+        <div>
+            <h1>Our Index Page</h1>
+        </div>
+    )
+}
+
+export default Index
+
